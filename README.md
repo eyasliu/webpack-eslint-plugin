eb407d3603caae0133f3b227c0c0c64302d6b2d0
liuyuesong <liuyuesong@kingsoft.com>
2016-11-11 16:26:43 +0800

    init webpack plugin

---
+++ README.md
@@ -0,0 +1,39 @@
+# webpack-eslint-plugin
+
+async run eslint for webpack plugin, won't block webpack compilation
+
+# install
+
+```
+npm i -S webpack-eslint-plugin
+```
+
+# usage
+
+```js
+// webpack.config.js
+import EslintPlugin from 'webpack-eslint-plugin'
+module.exports = {
+  // ...
+  plugins: [
+    // other plugins
+    new EslintPlugin({/*options*/})
+  ]
+}
+```
+
+# options
+
+You can pass [eslint](http://eslint.org/docs/developer-guide/nodejs-api#cliengine) options
+
+### `formate`(default: eslint stylish formatter)
+
+```js
+new EslintPlugin({
+  formate: require('eslint-friendly-formatter')
+})
+```
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * 异步执行eslint，不会阻塞webpack编译
+ */
+export default class EslintPlugin {
+    constructor(options){
+        this.engins = new CLIEngine(options);
+        this.options = Object.assign(
+            {
+                format: 'stylish'
+            },
+            options
+        );
+    }
+    apply(compiler){
+        compiler.plugin('done', (stats, cb) => {
+            const files = stats.compilation.fileDependencies.filter(item => !~item.indexOf('node_modules') && ~['.js', '.jsx'].indexOf(path.parse(item).ext));            
+            const results = CLIEngine.getErrorResults(this.engins.executeOnFiles(files).results);
+            this.printResults(results)
+        })
+    }
+    printResults(results){
+        let formatter;
+        try{
+            formatter = this.engins.getFormatter(this.options.format)
+        } catch(e) {
+            console.log(e.message);
+            return false;
+        }
+        console.log(formatter(results))
+        return false;
+    }
+}
